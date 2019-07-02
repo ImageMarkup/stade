6d4952dba08272ca1efeddab49517e36a6a242d4
Dan LaManna <dan.lamanna@kitware.com>
2019-07-01 21:59:43 -0400

    Remove duplicate installed app

--- stade/settings/base.py
+++ stade/settings/base.py
@@ -8,7 +8,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.sites",
     "django.contrib.staticfiles",
     "core",
     "tracker",
