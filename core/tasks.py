720421dbe000c9754d1321ea7701bd63adc5e2d8
Dan LaManna <dan.lamanna@kitware.com>
2019-07-02 15:42:32 -0400

    Add email templates, clean up scoring notification logic

--- core/tasks.py
+++ core/tasks.py
@@ -4,6 +4,7 @@ from celery import shared_task
 from celery.utils.log import get_task_logger
 from django.conf import settings
 from django.contrib.sites.models import Site
+from django.template.loader import render_to_string
 
 from core.models import Submission, Task
 from django.core.mail import send_mail
@@ -12,6 +13,30 @@ from isic_challenge_scoring.task3 import compute_metrics
 
 
 logger = get_task_logger(__name__)
+
+
+def notify_creator_of_scoring_attempt(submission):
+    context = {'submission': submission, 'url': f'https://{Site.objects.get_current().domain}'}
+    message = render_to_string(f'email/submission_{submission.status}.txt', context)
+    html_message = render_to_string(f'email/submission_{submission.status}.html', context)
+    subject_prefix = f'[{submission.approach.task.name}] '
+
+    if submission.status == 'succeeded':
+        subject = f'{subject_prefix}Submission succeeded (#{submission.id})'
+    elif submission.status == 'failed':
+        subject = f'{subject_prefix}Submission failed (#{submission.id})'
+    elif submission.status == 'internal_failure':
+        subject = f'{subject_prefix}Submission failed (internal error) (#{submission.id})'
+    else:
+        raise Exception(f'Encountered unexpected submission status {submission.status}.')
+
+    send_mail(
+        subject,
+        message,
+        settings.DEFAULT_FROM_EMAIL,
+        [submission.creator.email],
+        html_message=html_message,
+    )
 
 
 @shared_task
@@ -38,41 +63,14 @@ def score_submission(submission_id, notify=True):
         submission.score_history.create(
             score=submission.score, overall_score=submission.overall_score
         )
-        if notify:
-            from django.template.loader import render_to_string
-
-            context = {
-                'submission': submission,
-                'url': f'https://{Site.objects.get_current().domain}',
-            }
-            message = render_to_string('email/submission_succeeded.txt', context)
-            html_message = render_to_string('email/submission_succeeded.html', context)
-            send_mail(
-                f'{submission.approach.task.name} | Submission succeeded (#{submission.id})',
-                message,
-                settings.DEFAULT_FROM_EMAIL,
-                [submission.creator.email],
-                html_message=html_message,
-            )
     except ScoreException as e:
         submission.status = "failed"
         submission.fail_reason = e.args[0]
         submission.save()
-        if notify:
-            send_mail(
-                f"{settings.EMAIL_SUBJECT_PREFIX}Submission failed",
-                f"it dun work, {e.args[0]}",
-                settings.DEFAULT_FROM_EMAIL,
-                [submission.creator.email],
-            )
     except Exception:
         logger.exception(f"internal error scoring submission {submission.id}")
         submission.status = "internal_failure"
         submission.save()
-        if notify:
-            send_mail(
-                f"{settings.EMAIL_SUBJECT_PREFIX}Submission failed (internal error)",
-                f"It failed due to internal error, it will be rescored when fixed, or an administrator will contact you.",
-                settings.DEFAULT_FROM_EMAIL,
-                [submission.creator.email],
-            )
+    finally:
+        if notify and submission.status != 'scoring':
+            notify_creator_of_scoring_attempt(submission)
