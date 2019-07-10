adf4b497109ea1227f71b075a1df81df4d28d8c7
Dan LaManna <dan.lamanna@kitware.com>
2019-07-10 16:31:44 -0400

    Add infrastructure for testing locally

---
+++ stade/settings/testing.py
@@ -0,0 +1,20 @@
+import os
+
+from .base import *  # noqa: F401, F403
+
+SECRET_KEY = 'insecuresecret'
+DEBUG = True
+ALLOWED_HOSTS = []
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stade',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),
+        'PORT': '5432',
+    }
+}
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+MEDIA_ROOT = 'uploads/'
+MEDIA_URL = '/uploads/'
