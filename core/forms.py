0bc24fec7f98675c424f2211fb956e8efffbd2f3
Joanne Tsai <joanne.tsai@kitware.com>
2019-07-16 12:31:10 -0400

    Fix invite notifications
    Errors were global messages, changed to now be local.

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,6 @@
 from allauth.account.forms import SignupForm
 from django import forms
 from django.conf import settings
-from django.contrib import messages
 from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 from django.utils.translation import gettext_lazy as _
@@ -54,21 +53,13 @@ class CreateInvitationForm(forms.ModelForm):
     def clean(self):
         team = get_object_or_404(self.request.user.teams, pk=self.team_id)
         if team.users.filter(email=self.cleaned_data['recipient']).exists():
-            messages.add_message(
-                self.request,
-                messages.ERROR,
-                f'{self.cleaned_data["recipient"]} is already in the team',
-            )
-            raise forms.ValidationError('User is already in the team')
+            raise forms.ValidationError(f'{self.cleaned_data["recipient"]} is already in the team')
         if TeamInvitation.objects.filter(
             recipient=self.cleaned_data['recipient'], team_id=team.id
         ).exists():
-            messages.add_message(
-                self.request,
-                messages.ERROR,
-                f'{self.cleaned_data["recipient"]} has already been invited',
+            raise forms.ValidationError(
+                f'{self.cleaned_data["recipient"]} has already been invited'
             )
-            raise forms.ValidationError('User has already been invited')
 
 
 class CreateTeamForm(forms.ModelForm):
