01e8dd87540681156a575dd66f58d973674c2a83
Brian Helba <brian.helba@kitware.com>
2019-11-18 18:19:50 -0500

    Always run in production mode by default

--- manage.py
+++ manage.py
@@ -4,7 +4,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stade.settings.development')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stade.settings.production')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
