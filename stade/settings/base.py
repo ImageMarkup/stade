14bf981ab1032235d09449d4c5d49aa80ff08522
Dan LaManna <dan.lamanna@kitware.com>
2020-01-27 13:20:39 -0500

    Add django-s3-file-field as a dependency

--- stade/settings/base.py
+++ stade/settings/base.py
@@ -10,6 +10,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    's3_file_field',
     'core',
     'tracker',
     'corsheaders',
