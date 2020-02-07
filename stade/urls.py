9b3aae9985966f1fad64204df5a270f8bbfbe5f6
Brian Helba <brian.helba@kitware.com>
2020-02-06 21:15:46 -0500

    Set up MinIO for development

--- stade/urls.py
+++ stade/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
@@ -34,5 +33,3 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
-
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
