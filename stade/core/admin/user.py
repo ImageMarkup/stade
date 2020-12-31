4e6e67335f1a0c786260a7a945df8efcc5221cab
Brian Helba <brian.helba@kitware.com>
2020-12-31 15:46:54 -0500

    Minor code cleanup

--- stade/core/admin/user.py
+++ stade/core/admin/user.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from django.utils.safestring import mark_safe
+from django_admin_display import admin_display
 
 from .team import TeamInline
 
@@ -9,21 +10,20 @@ from .team import TeamInline
 class UserAdmin(BaseUserAdmin):
     ordering = ['-date_joined']
     inlines = [TeamInline]
-    list_display = ['date_joined', '_email', 'name', 'is_staff']
+    list_display = ['date_joined', 'formatted_email', 'name', 'is_staff']
 
     def name(self, obj):
         return f'{obj.first_name} {obj.last_name}'
 
     @mark_safe
-    def _email(self, obj):
+    @admin_display(short_description='Email')
+    def formatted_email(self, obj):
         if not obj.is_active:
             user = f'<strike>{obj.email}</strike>'
         else:
             user = obj.email
 
         return user
-
-    _email.short_description = 'Email'
 
 
 # Re-register UserAdmin
