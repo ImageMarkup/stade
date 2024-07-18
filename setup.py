9275d5bc731e7421cc0211d9405b8d75d8e1bd6b
Dan LaManna <dan.lamanna@kitware.com>
2024-07-18 13:08:35 -0400

    Upgrade to django 5

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=[
         'celery',
         'dictdiffer',
-        'django<4.2.0',
+        'django<6.0',
         'django-allauth',
         'django-configurations[database,email]',
         'django-extensions',
